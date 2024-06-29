e8f14bb713b6e26bbc04662f793c2ab48b46eb37
HyunSu1768 <azxcv1768@gmail.com>
2024-06-29 19:59:27 +0900

    first commit

---
+++ src/main/java/com/example/gocd_test_repository/GocdTestRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.gocd_test_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GocdTestRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GocdTestRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gocd_test_repository/GocdTestRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gocd_test_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GocdTestRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
